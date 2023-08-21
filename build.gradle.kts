9be16394235291d58c5026777586b907f9bd65d8
BrianDYKim <18k7102dy@naver.com>
2023-08-21 23:55:31 +0900

    [ADD] gradle 설정 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,31 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val mockkVersion = "1.12.0"
+val kotestVersion = "5.5.5"
+
 plugins {
-    id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.2"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.jetbrains.kotlin.plugin.allopen")
+    id("org.jetbrains.kotlin.plugin.noarg")
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+
+    kotlin("kapt")
+    idea
 }
 
-group = "com.tutoring"
-version = "0.0.1-SNAPSHOT"
+allOpen {
+    // Spring Boot 3.0.0
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+group = "com.tutoring.lunchbot"
+version = "0.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -19,10 +36,38 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Kotlin Standard Library
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.module:jackson-module-afterburner")
+
+    // SpringBoot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Spring actuator
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Jakarta (SpringBoot version >= 3.0.0)
+//    kapt ("jakarta.annotation:jakarta.annotation-api")
+//    kapt ("jakarta.persistence:jakarta.persistence-api")
+
+    // Database
+//    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // mockk
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // for kotest core jvm assertions
+    testImplementation("io.kotest:kotest-property:$kotestVersion") // for kotest property test
+
+    // Annotation Processing Tool
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
@@ -30,6 +75,10 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.INCLUDE
 }
 
 tasks.withType<Test> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,20 @@
 rootProject.name = "lunch-bot"
+
+pluginManagement {
+    val kotlinVersion = "1.8.22"
+    val springBootVersion = "3.1.2"
+    val dependencyManagementVersion = "1.1.2"
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version dependencyManagementVersion
+        id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
+        id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
+
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+
+        kotlin("kapt") version kotlinVersion
+    }
+}
